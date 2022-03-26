4c91b8c02fd49b3c168fa7b7d4245f2f1aa1ec27
Hitharlin <98156708+Hitharlin@users.noreply.github.com>
2022-03-26 17:02:03 +0400

    Update index.js
    feat(winner): doesCartWinPrize

--- index.js
+++ index.js
@@ -4,7 +4,16 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  
+  let sum = 0
+    const POW = Math.pow(10, 10)
+
+    for ( let i = 0; i < cartItemsPrices.length; i++ ) {
+        let num = Number( cartItemsPrices[i] ) * POW
+        sum = sum + num
+    }
+    sum /= POW;
+
+    return sum === WINNING_SUM;
 }
 
 module.exports = doesCartWinPrize;
