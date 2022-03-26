faff67579aac46ef773d6051b7cf2679301cb83b
Hitharlin <98156708+Hitharlin@users.noreply.github.com>
2022-03-26 17:07:01 +0400

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const WINNING_SUM = 123.40;
 @param {Array} cartItemsPrices
 */
 function doesCartWinPrize(cartItemsPrices) {
-  let sum = 0
-    const POW = Math.pow(10, 10)
+  let sum = 0;
+    const POW = 100;
 
     for ( let i = 0; i < cartItemsPrices.length; i++ ) {
         let num = Number( cartItemsPrices[i] ) * POW
